6bff37d602ae387f70fb1eb680d25051d61b5924
Ariel_Naiman <a0527633600@gmail.com>
2025-10-26 15:30:26 +0200

    create_card, compare_cards

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 def create_card(rank:str, suite:str) -> dict:
     value = {"A":14, "1":1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":11, "Q":12, "k":13}
-    return {"rank":rank, "suite":suite, "value":value[rank]}
-
-def create_card_2(rank:str, suite:str) -> dict:
-    
+    return {"rank":rank, "suite":suite, "value":value[rank]}    
 
 def compare_cards(p1_card: dict, p2_card: dict) -> str:
     if p1_card["value"] > p2_card["value"]:
@@ -11,8 +8,3 @@ def compare_cards(p1_card: dict, p2_card: dict) -> str:
     if p1_card["value"] < p2_card["value"]:
         return "p2"
     return "WAR"
-
-def create_deck() -> list[dict]:
-    
-
-print(create_card("A", "S"))
